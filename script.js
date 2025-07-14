418d73e4bf48dfd12974d3a77bdf4e5d34a25387
Vishnu8767 <boddu.reddy@s.amity.edu>
2025-07-14 13:50:32 +0530

    createdfunction

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function sendMessage() {
   const userInput = document.getElementById("user-input");
   const chatBox = document.getElementById("chat-box");
-
+  const userText = userInput.value.trim();
+  if (!userText) return;
