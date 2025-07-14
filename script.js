da1046c73d94d120b17692b7866783ffde06b574
Vishnu8767 <boddu.reddy@s.amity.edu>
2025-07-14 13:51:49 +0530

    user_input

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ function sendMessage() {
   const userInput = document.getElementById("user-input");
   const chatBox = document.getElementById("chat-box");
   const userText = userInput.value.trim();
-  if (!userText) return;
+  if (!userText) return;  
+   chatBox.innerHTML += `<div><strong>You:</strong> ${userText}</div>`;
+
